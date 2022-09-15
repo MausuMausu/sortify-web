c290fc35e35760e02a5db108084921425a320ade
ggrostins <gundars.grostins@helmes.com>
2022-09-15 12:17:20 +0300

    added list creation

---
+++ pages/list/_id.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>{{ $route.params.id }}</h1>
+    <nuxt-child/>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/new-list.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-text-field
+        v-model="listName"
+        label="List Name"
+      ></v-text-field>
+      <!-- <v-btn @click="createList" color="primary">Create List</v-btn> -->
+      <v-btn router exact :to="`list/${listName}`"  color="primary">Create List</v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      listName: ""
+    }
+  },
+  methods: {
+    createList () {
+      this.$router.push({ name: 'list', params: { id: this.listName } })
+    }
+  }
+
+}
+</script>
